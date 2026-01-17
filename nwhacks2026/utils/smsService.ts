b27bfe090dfe14d20f7e44af9884e525797abdef
HaydenChan8619 <haydenphchan@gmail.com>
2026-01-17 15:51:00 -0800

    add praise text

--- nwhacks2026/utils/smsService.ts
+++ nwhacks2026/utils/smsService.ts
@@ -16,3 +16,19 @@ export const sendShameText = async () => {
         return false;
     }
 };
+
+export const sendPraiseText = async () => {
+    try {
+        console.log("🚀 Triggering Shame Text...");
+
+        // Replace with your IFTTT/Zapier/API URL
+        await fetch('https://hooks.zapier.com/hooks/catch/26089976/ugnt55f/', { method: 'POST' });
+
+        // For Hackathon Demo:
+        Alert.alert("🔥 PENALTY TRIGGERED", "Text sent: 'Hayden missed his habit, check in on him!'");
+        return true;
+    } catch (error) {
+        console.error("Failed to send text:", error);
+        return false;
+    }
+};
